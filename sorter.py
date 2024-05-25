74a188ec654186f5ca34a4801808a46badc94b50
Alex Gulliver <alexgulliver@Alexs-MacBook-Pro.local>
2024-05-25 18:31:05 +0100

    Added check to ignore folders in sorting folder

--- sorter.py
+++ sorter.py
@@ -21,32 +21,35 @@ class ImageSorter:
         sort_folder_path = os.path.join(os.path.dirname(__file__), sortfolder)
         for file in os.listdir(sort_folder_path):
             filename = os.fsdecode(file)
-            filepath = sortfolder + "/" + filename
-            image = Image.open(filepath)
-            exifdata = image.getexif()
-            if exifdata and 306 in exifdata:
-                year = exifdata[306][0:4]
-                month = exifdata[306][5:7]
-                print(f"File: {filename}")
-                print(f"Year: {year}")
-                print(f"Month: {month}")
-                print(f"Datetime: {exifdata[306]}\n")
+            
+            # Checks the file is not a folder
+            if "." in filename:
+                filepath = sortfolder + "/" + filename
+                image = Image.open(filepath)
+                exifdata = image.getexif()
+                if exifdata and 306 in exifdata:
+                    year = exifdata[306][0:4]
+                    month = exifdata[306][5:7]
+                    print(f"File: {filename}")
+                    print(f"Year: {year}")
+                    print(f"Month: {month}")
+                    print(f"Datetime: {exifdata[306]}\n")
 
-                if os.path.isdir(year):
-                    pass
-                else:
-                    os.mkdir(year)
+                    if os.path.isdir(year):
+                        pass
+                    else:
+                        os.mkdir(year)
 
-                if os.path.isdir(os.path.join(year, month)):
-                    pass
-                else:
-                    os.mkdir(os.path.join(year, month))
+                    if os.path.isdir(os.path.join(year, month)):
+                        pass
+                    else:
+                        os.mkdir(os.path.join(year, month))
 
-                sorted_path = os.path.join(year, month) + "/" + filename
+                    sorted_path = os.path.join(year, month) + "/" + filename
 
-                os.rename(filepath, sorted_path)
-
-                # Create folder with that year/month structure
+                    os.rename(filepath, sorted_path)
+            else:
+                pass
 
 
 image_sorter = ImageSorter()
@@ -54,7 +57,7 @@ image_sorter = ImageSorter()
 sort_folder_created = image_sorter.init_sorting_folder(sortfolder)
 
 if sort_folder_created:
-    print(f"{sortfolder} folder created.")
+    print(f"Specified sort folder does not exist. Place image files inside /{sortfolder} and re-run script.")
 else:
     print(f"{sortfolder} folder already exists.")
     image_sorter.sort_images(sortfolder)
